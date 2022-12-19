3efed6a08f2d5c1254b5dfbe010f7b843aa45dce
Mattias Holm <mattias.holm@live.com>
2022-12-19 19:56:33 +0100

    Updated validate.py

--- validate.py
+++ validate.py
@@ -5,12 +5,30 @@ import re
 
 path = 'recipes'
 files = os.listdir(path)
+file_count = len(files)
 
-rating1 = []
-rating2 = []
-rating3 = []
-rating4 = []
-rating5 = []
+ratings = {
+    1: {
+        'stars': '★☆☆☆☆',
+        'drinks': []
+    },
+    2: {
+        'stars': '★★☆☆☆',
+        'drinks': []
+    },
+    3: {
+        'stars': '★★★☆☆',
+        'drinks': []
+    },
+    4: {
+        'stars': '★★★★☆',
+        'drinks': []
+    },
+    5: {
+        'stars': '★★★★★',
+        'drinks': []
+    }
+}
 
 for file in files:
     extension = os.path.splitext(file)[-1]
@@ -23,64 +41,51 @@ for file in files:
     if extension != '.md':
         level = 'WARNING'
         message = 'Incorrect file extension'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
     if header != f'# {name}\n':
         level = 'WARNING'
         message = 'Incorrect name header'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
     if not '## Ingredients\n' in content:
         level = 'WARNING'
         message = 'Incorrect ingredients header'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
     if not '## Instructions\n' in content:
         level = 'WARNING'
         message = 'Incorrect instructions header'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
     if not '## Glassware\n' in content:
         level = 'WARNING'
         message = 'Incorrect glassware header'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
     if not '## Rating\n' in content:
         level = 'WARNING'
         message = 'Incorrect rating header'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
     if not re.match('^- ★[★☆]{4}$', rating):
         level = 'WARNING'
         message = 'Incorrect rating'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
     if re.search('<!--', str(content)):
         level = 'INFO'
         message = 'Contains comments'
-        print(f'{level:<15} {name:<30} {message}')
+        print(f'{name:<30} {level:<15} {message}')
 
-    match rating:
-        case "- ★☆☆☆☆":
-            rating1.append(name)
+    ratings[rating.count('★')]['drinks'].append(name)
 
-        case "- ★★☆☆☆":
-            rating2.append(name)
-
-        case "- ★★★☆☆":
-            rating3.append(name)
-
-        case "- ★★★★☆":
-            rating4.append(name)
-
-        case "- ★★★★★":
-            rating5.append(name)
-
-print(f'\nNumber of drinks in total:\n{len(files)}')
+print(f'\nNumber of drinks in total:\n{file_count}')
 
 print('\nNumber of drinks per rating:')
-print(f'★☆☆☆☆\t\t{len(rating1)}\t\t{round(len(rating1) / len(files) * 100, 2)}%')
-print(f'★★☆☆☆\t\t{len(rating2)}\t\t{round(len(rating2) / len(files) * 100, 2)}%')
-print(f'★★★☆☆\t\t{len(rating3)}\t\t{round(len(rating3) / len(files) * 100, 2)}%')
-print(f'★★★★☆\t\t{len(rating4)}\t\t{round(len(rating4) / len(files) * 100, 2)}%')
-print(f'★★★★★\t\t{len(rating5)}\t\t{round(len(rating5) / len(files) * 100, 2)}%')
+
+for rating in ratings.values():
+    stars = rating['stars']
+    count = len(rating['drinks'])
+    percentage = round(count / file_count * 100, 2)
+    print(f'{stars:<30} {count:<15} {percentage}%')
