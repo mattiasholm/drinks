54b481f88c9a9cd02402430c1a08062e2d736fa5
Mattias Holm <mattias.holm@live.com>
2024-12-23 11:40:50 +0100

    Updated dependencies

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -4,11 +4,11 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className='container'>
